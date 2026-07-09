cbba917a921fa1e103c2c4ea9d614b931074f6bc
pascal <emmadera32@gmail.com>
2026-07-09 20:39:29 +1000

    updated main.tf file

--- main.tf
+++ main.tf
@@ -54,7 +54,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "my_subnet" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
-    availability_zone = "us_east-1a"
+    availability_zone = "us-east-1a"
     map_public_ip_on_launch = true
 
     tags = {
