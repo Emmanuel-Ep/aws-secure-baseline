a80ba002959f30f3d3cc3f36e18da129cc1fbbc7
pascal <emmadera32@gmail.com>
2026-07-09 19:53:52 +1000

    added custom-vpc and subnet

--- main.tf
+++ main.tf
@@ -41,7 +41,22 @@ resource "aws_security_group" "web_sg" {
     }
 }
 
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+    enable_dns_support = true
+    enable_dns_hostnames = true
 
+    tags = {
+        Name = "web_vpc"
+    }
+}
+
+resource "aws_subnet" "my_subnet" {
+    vpc_id = aws_vpc.main.id
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "us_east-1a"
+    map_public_ip_on_launch = true
+}
 
    
     
